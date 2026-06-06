0a0d3da5c336e58982cd4e34abf25724f11fd096
MD. Shakil Hossain <sh698681@example.com>
2026-06-06 15:41:04 +0600

    Add frontend and finalize merged repository

--- SmartPOS.API/Program.cs
+++ SmartPOS.API/Program.cs
@@ -20,9 +20,12 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            policy.WithOrigins(
+          "http://localhost:4200",
+          "http://10.220.194.66:4200"
+      )
+      .AllowAnyHeader()
+      .AllowAnyMethod();
         });
 });
 
